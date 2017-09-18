5698f7b12681c6476f51fdbab21b4f4447de15db
Andrew Bedford <andrew.bedford.1@ulaval.ca>
2017-09-18 00:10:09 -0400

    Parse arguments
    Added the two following commands:
     --help
     --text [.v file] [lemma/theorem name]
    
    The latter converts the Coq proof of [lemma/theorem name] in file [file] to text (i.e., classical proof).

--- src/main/java/coqatoo/Main.java
+++ src/main/java/coqatoo/Main.java
@@ -1,8 +1,47 @@
 package coqatoo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
+    static final Map<String, List<String>> parameters = new HashMap<String, List<String>>();
 
     public static void main(String[] args) {
-	// write your code here
+        parseArguments(args);
+        if (parameters.isEmpty() || parameters.containsKey("-help")) {
+            System.out.println("Options:");
+            System.out.println("-text [.v file] [lemma/theorem name]\t\t Converts the Coq proof of [lemma/theorem name] in file [file] to text (i.e., classical proof).");
+        }
+        else if (parameters.containsKey("-text")) {
+            String filePath = parameters.get("-text").get(0);
+            String lemmaName = parameters.get("-text").get(1);
+            System.out.println(filePath + " " + lemmaName);
+        }
+    }
+
+    private static void parseArguments(String[] args) {
+        List<String> options = null;
+        for (int i = 0; i < args.length; i++) {
+            final String a = args[i];
+
+            if (a.charAt(0) == '-') {
+                if (a.length() < 2) {
+                    System.err.println("Error at argument " + a);
+                    return;
+                }
+
+                options = new ArrayList<String>();
+                parameters.put(a.substring(1), options);
+            }
+            else if (options != null) {
+                options.add(a);
+            }
+            else {
+                System.err.println("Illegal parameter usage");
+                return;
+            }
+        }
     }
 }
