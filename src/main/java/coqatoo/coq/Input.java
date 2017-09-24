e22d72517fcecd05a1b106a18484c74562441f57
Andrew Bedford <andrew.bedford.1@ulaval.ca>
2017-09-23 23:02:48 -0400

    Determine the input types
    Supports only the bare mimum to test our algorithm on conj_imp_equiv.

--- src/main/java/coqatoo/coq/Input.java
+++ src/main/java/coqatoo/coq/Input.java
@@ -2,10 +2,28 @@ package coqatoo.coq;
 
 public class Input {
     private String _value;
+    private InputType _type;
 
     public Input(String value) {
-        _value = value;
+        _value = value.trim();
+        _type = determineType(_value);
     }
 
     public String getValue() { return _value; }
+
+    private InputType determineType(String value) {
+        if (value.startsWith("Abort")) { return InputType.ABORT; }
+        if (value.startsWith("apply")) { return InputType.APPLY; }
+        if (value.startsWith("assumption")) { return InputType.ASSUMPTION; }
+        if (value.startsWith("-") || value.startsWith("+") || value.startsWith("*")) { return InputType.BULLET; } //TODO Add support for { } bullets
+        if (value.startsWith("intros")) { return InputType.INTROS; }
+        if (value.startsWith("intro")) { return InputType.INTRO; }
+        if (value.startsWith("inversion")) { return InputType.INVERSION; }
+        if (value.startsWith("Lemma")) { return InputType.LEMMA; }
+        if (value.startsWith("Proof")) { return InputType.PROOF; }
+        if (value.startsWith("split")) { return InputType.SPLIT; }
+        if (value.startsWith("Qed")) { return InputType.QED; }
+
+        return InputType.UNKNOWN;
+    }
 }
---
+++ src/main/java/coqatoo/coq/InputType.java
@@ -0,0 +1,16 @@
+package coqatoo.coq;
+
+public enum InputType {
+    ABORT,
+    APPLY,
+    ASSUMPTION,
+    BULLET,
+    INTRO,
+    INTROS,
+    INVERSION,
+    LEMMA,
+    PROOF,
+    SPLIT,
+    UNKNOWN,
+    QED
+}
