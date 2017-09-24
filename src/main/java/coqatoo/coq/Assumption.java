86c1df4f40084b0cfccf8c5d9efd1cb7938ed0b2
Andrew Bedford <andrew.bedford.1@ulaval.ca>
2017-09-24 00:23:05 -0400

    Extract output assumptions

---
+++ src/main/java/coqatoo/coq/Assumption.java
@@ -0,0 +1,10 @@
+package coqatoo.coq;
+
+public class Assumption {
+    private String _value;
+
+    public Assumption(String value) {
+        _value = value;
+    }
+
+}
--- src/main/java/coqatoo/coq/Output.java
+++ src/main/java/coqatoo/coq/Output.java
@@ -1,11 +1,51 @@
 package coqatoo.coq;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Output {
     private String _value;
+    private Set<Assumption> _assumptions;
 
     public Output(String value) {
         _value = value;
+        _assumptions = determineAssumptions(_value);
     }
 
     public String getValue() { return _value; }
+    public Set<Assumption> getAssumptions() { return _assumptions; }
+
+    private Set<Assumption> determineAssumptions(String value) {
+        Set<Assumption> assumptions = new HashSet<>();
+
+        String[] t = value.split("============================\n ");
+        if (t.length > 1) {
+            t = t[0].split("\n  \n ");
+            if (t.length > 1) {
+                t = t[1].split("\n ");
+
+                for (String s : t) {
+                    if (!s.trim().isEmpty()) {
+
+                        //If the assumption has the form "P, Q : Prop", then add two assumptions "P : Prop" and "Q : Prop"
+                        String[] namesAndType = s.split(" : ");
+                        String type = namesAndType[1];
+                        String[] names = namesAndType[0].split(", ");
+                        if (names.length > 1) {
+                            for (String name : names) {
+                                assumptions.add(new Assumption(name + " : " + type));
+                            }
+                        }
+                        else {
+                            assumptions.add(new Assumption(s));
+                        }
+
+                    }
+                }
+            }
+
+        }
+
+        return assumptions;
+    }
 }
